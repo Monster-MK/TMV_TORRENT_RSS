c81119ea006c6129b1edf601fa385a5537e53c82
Jeeva8874 <110565010+Jeeva8874@users.noreply.github.com>
2024-12-08 00:39:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 from os import getenv as genv
 from pyrogram import Client
 
-API_ID = genv("API_ID", "")
+API_ID = genv("API_ID", "22977776")
 
-API_HASH = genv("API_HASH", "")
+API_HASH = genv("API_HASH", "2ac7223d720bdeec757cbc88ced57224")
 
-BOT_TOKEN = genv("BOT_TOKEN", "")
+BOT_TOKEN = genv("BOT_TOKEN", "7692272341:AAEcqZkFIx6Y6iljv96xt3SXSNwGY4_V8O4")
 
-SUPPORT_GROUP = genv("SUPPORT_GROUP", "madxbotzsupport")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "TGHelpingGroup")
 
-UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "MadxBotz")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "PlayTamilX")
 
-GROUP_ID = int(genv("GROUP_ID", ""))  # add group id where your leech is added
+GROUP_ID = int(genv("GROUP_ID", "-1002341945360"))  # add group id where your leech is added
 
 DATABASE_URL = genv(
     "DATABASE_URL",
@@ -20,14 +20,14 @@ DATABASE_URL = genv(
 
 USER_SESSION_STRING = genv(
     "USER_SESSION_STRING",
-    "",
+    "BQFenPAAj0vR59QL_tH7p95B26NNPgETKPYRFf8jKfxPt2_rVChFajjDhjX101ZWItMub9Yo97IWN99spQoJ_ZkBjPxiyZJsSSX_AFrdbrdPitpEc8-NBul23Pz-j2Oo4Xw0i17iTnSf2oN0XnIYxh7VIkyB53ZR2UJErrAIvmkU9Im8on4rfifSyK1iXvRWIVFPs6e2Jc3xPGX_gMv6GOsLwb9y6cZ6Hni_uCrYbrV-A0rDgpmaa6kJXNOb54CT2FK7W275YYA8o2HdvXQlDmmKZpdtrjs6ekLdEcTv8Sbr-EhD4XEYqg86AcDPWrDOiXrLLr3EfKkRYu_E5Gwq4eEt05hzUQAAAAGTFHWXAA",
 )
 
 SERVER_URL = genv("SERVER_URL", "")
 
 
 RSS_CHAT = int(
-    genv("RSS_CHAT", "")
+    genv("RSS_CHAT", "-1002335041395")
 )  # add the channel id where the torrent files need to be sent
 
 BASE_URL = genv(
@@ -43,7 +43,7 @@ BASE_URL = genv(
 START_TXT = """<b>Hello {}, I am a Scrapper Bot!.
 ๏ I can scrap links from 1tamilmv and update it in Bot..
 ๏ Click on the help menu button below to get information about my commands.
-๏ Powered By @MadxBotz</b>"""
+๏ Powered By @TamilXLeech</b>"""
 
 
 HELP_TXT = """Send any Movie Name and I will provide torrent links.
@@ -58,8 +58,8 @@ Updates - ⍟ @MadxBotz"""
 ABOUT_TXT = """<b>╔════❰ Tʙ Sʜᴏʀᴛɴᴇʀ Bᴏᴛ ❱═══❍
 ║ ┏━━━━━━━━━❥
 ║ ┣ Mʏ ɴᴀᴍᴇ -> {}
-║ ┣ Uᴘᴅᴀᴛᴇꜱ -> <a href="tg://MadxBotz">••Bᴏᴛs••</a>
-║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> <a href="tg://madxbotzsupport"> Bᴏᴛs Sᴜᴩᴩᴏʀᴛ</a>
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> <a href="tg://TamilXLeech">••Bᴏᴛs••</a>
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> <a href="tg://TGHelpingGroup"> Bᴏᴛs Sᴜᴩᴩᴏʀᴛ</a>
 ║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
 ║ ┗━━━━━━━━━❥
 ╚═════❰ @ ❱═════❍</b>"""
