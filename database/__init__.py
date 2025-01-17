1de81f64994adda08d6c6d472d9737485ab637d3
Ananth8874 <heart81570801@gmail.com>
2025-01-17 12:18:26 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -97,7 +97,7 @@ async def send_new_link_notification(links):
 
                         await User.send_message(
                             chat_id=GROUP_ID,
-                            text="/qbleech",
+                            text="/qbleech1",
                             reply_to_message_id=sent_msg.id,
                         )
 
