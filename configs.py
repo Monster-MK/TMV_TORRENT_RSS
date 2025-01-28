5307938a446eb9ac6fc7ea0cd8147f516612d7a8
Ananth8874 <heart81570801@gmail.com>
2025-01-28 12:29:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ SUPPORT_GROUP = genv("SUPPORT_GROUP", "TGHelpingGroup")
 
 UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "TamizhFiles")
 
-GROUP_ID = int(genv("GROUP_ID", "-1002470524159"))
+GROUP_ID = int(genv("GROUP_ID", "-1002325685641"))
 
 DATABASE_URL = genv(
     "DATABASE_URL",
