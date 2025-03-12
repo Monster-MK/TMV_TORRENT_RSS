a42440c5aab932ed171d0a45fa9f67bcfa43d007
Ananth8874 <heart81570801@gmail.com>
2025-03-12 22:58:04 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -17,27 +17,21 @@ message_lock = asyncio.Lock()
 
 executor = ThreadPoolExecutor()
 
-"""async def fetch(url):
+async def fetch(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
-        "Referer": "https://www.google.com/",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
     }
 
     loop = asyncio.get_event_loop()
     try:
-        response = await loop.run_in_executor(
-            executor, lambda: requests.get(url, headers=headers, timeout=10)
-        )
+        response = await loop.run_in_executor(executor, requests.get, url, headers)
         response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching {url}: {str(e)}")
-        return None"""
+        return None
 
-async def fetch(url):
+"""async def fetch(url):
     scraper = cloudscraper.create_scraper()  # Create a scraper instance to bypass Cloudflare protection
 
     headers = {
@@ -52,7 +46,7 @@ async def fetch(url):
         return response.text
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching {url}: {str(e)}")
-        return None
+        return None"""
 
 def get_size_in_bytes(size_str):
     size_str = size_str.lower()
