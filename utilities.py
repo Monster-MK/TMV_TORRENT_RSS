f15d664bea665240ad21f2eed8170f09a79d4e52
Ananth8874 <heart81570801@gmail.com>
2025-03-16 18:30:33 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import asyncio, logging, aiohttp
+import cloudscraper  # Import CloudScraper
 from bs4 import BeautifulSoup
 import re
 from datetime import datetime
@@ -13,19 +14,20 @@ from urllib.parse import urlparse
 
 message_lock = asyncio.Lock()
 
-
 executor = ThreadPoolExecutor()
 
-
 async def fetch(url):
+    scraper = cloudscraper.create_scraper()  # Create a scraper instance to bypass Cloudflare protection
+
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
     }
 
     loop = asyncio.get_event_loop()
     try:
-        response = await loop.run_in_executor(executor, requests.get, url, headers)
-        response.raise_for_status()
+        # Pass the scraper.get function with arguments to run_in_executor
+        response = await loop.run_in_executor(executor, lambda: scraper.get(url, headers=headers))
+        response.raise_for_status()  # Raise an error for HTTP errors (4xx, 5xx)
         return response.text
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching {url}: {str(e)}")
