b54d0d7aee283d769039fc23ea8b9cbff1ecf09a
MƠƝƧƬЄƦ MƘ 🔥 <hardikpandya4849@gmail.com>
2025-08-08 07:18:36 +0530

    Update tmv_scraper.py

--- tmv_scraper.py
+++ tmv_scraper.py
@@ -1,7 +1,16 @@
 import feedparser
 
 def fetch_rss(url):
+    # RSS feed parse
     feed = feedparser.parse(url)
-    # RSS list-ஐ 10 items மட்டும் எடுக்க
+
+    # 10 items மட்டும் எடுக்க
     entries = feed.entries[:10]
+
+    # பெரிய feed object memory release
+    try:
+        del feed
+    except:
+        pass
+
     return entries
