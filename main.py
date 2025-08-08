40d9c4d3564540302d0bd8b0edc003cf2a6847a4
MƠƝƧƬЄƦ MƘ 🔥 <hardikpandya4849@gmail.com>
2025-08-08 07:15:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from tmv_scraper import get_torrent_data
 def main():
     # உங்கள் torrent விவரக்கை url இட்டுக்கோங்க
     url = "https://example.com/torrent-page"
+
+    # Torrent data process
     get_torrent_data(url)
+
+    # Memory clear (avoid R14 error in Heroku)
+    try:
+        del url
+    except:
+        pass
 
 if __name__ == "__main__":
     main()
